e21e1cdb35291e5ec7aeee62ce37506fedafbd6d
purevb <purevbaatar221@gmail.com>
2024-05-06 14:54:57 +0800

    cors nemev

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const Ciity = require("./models/City");
 require('dotenv').config();
 const  corsConfig={
-  origin:["https://naruto-api-gamma.vercel.app/"],
+  origin:["http://ec2-54-226-151-184.compute-1.amazonaws.com:8080/"],
   methods:["POST",'GET'],
   credentials:true
 }
